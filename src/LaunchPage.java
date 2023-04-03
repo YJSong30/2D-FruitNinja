211a3a0ce8e9ad0adf9be52dd0aeb27a9864ca3a
Young <youngsong1230@gmail.com>
2023-04-03 02:09:48 -0700

    Added background music for main menu screen

--- src/LaunchPage.java
+++ src/LaunchPage.java
@@ -1,8 +1,13 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+
 
 public class LaunchPage implements ActionListener{
 	
@@ -11,6 +16,11 @@ public class LaunchPage implements ActionListener{
 	
 	
 	LaunchPage(){
+		
+		String BGMpath = "media/Maplestory Theme Music - Intro.wav";
+		WAVplayInstance BGMplayer = new WAVplayInstance();
+		
+		BGMplayer.playWAV(BGMpath, true);
 		
 		myButton.setBounds(300, 400, 200, 40);
 		myButton.setFocusable(false);
@@ -23,8 +33,35 @@ public class LaunchPage implements ActionListener{
 		frame.setLayout(null);
 		frame.setVisible(true);
 		
+		
 	}
+	
+	class WAVplayInstance {
+        void playWAV(String WAVpath, boolean loopState){
+                 try {
+                         File pathString = new File(WAVpath);
+                          if(pathString.exists()){ 
+                                  AudioInputStream WAVstream = AudioSystem.getAudioInputStream(pathString);
+                                  Clip tempIn = AudioSystem.getClip();
+                                  tempIn.open(WAVstream);
+                                  if (loopState==false) {
+                                	  
+                                  }
+                                  else {
+                                	  tempIn.loop(Clip.LOOP_CONTINUOUSLY);
+                                  }
+                                  tempIn.start();
 
+                           }
+                          else{
+                                   System.out.println("WAV path nonexistent!");
+                                }
+                }
+                catch (Exception e){
+                           System.out.println(e);
+                     }
+           }
+}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
